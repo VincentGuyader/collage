7aacbbc9dffc129331adfd21efd292b94e16f620
Romain Francois <romain@r-enthusiasts.com>
2016-09-13 21:39:46 +0200

    trimming unused code

--- R/plotraster.R
+++ R/plotraster.R
@@ -1,18 +1,16 @@
-# -*- coding: utf-8 -*-
-#' @encoding UTF-8
-#' @title plotraster
-#' @description affiche une image raster dans la fenêtre graphique
+#' plotraster
+#'
+#' affiche une image raster dans la fenêtre graphique
 #' @param img l 'image (en array)
 #' @param verbose booleen qui rend la fonction bavarde
-#' @importFrom graphics rasterImage
+#' @importFrom graphics rasterImage par plot
 #' @export
 plotraster <- function(img, verbose = TRUE) {
-    # print('on plot')
-  img <- aperm(img, c(2, 1, 3))
-    ydim <- attributes(img)$dim[1]  # Image dimension y-axis
-    xdim <- attributes(img)$dim[2]  # Image dimension x-axis
-    par(bg = "white")
-    plot(c(0, xdim), c(0, ydim), type = "n", xlab = "", ylab = "", bty = "n", axes = FALSE)
+    width <- ncol(img)
+    height <- nrow(img)
+    par(bg = "white", mar = c(.5,.5,.5,.5))
+
+    plot(xlim = c(0, width), ylim = c(0, height), type = "n", xlab = "", ylab = "", bty = "n", axes = FALSE)
     if (verbose) {
         message(paste("        masque OK "))
     }
