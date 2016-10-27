a5027e191c547e557004dcdc5cee278d1d69c03f
Romain Francois <romain@r-enthusiasts.com>
2016-10-27 09:12:26 +0200

    small changes on collage

--- src/collage.cpp
+++ src/collage.cpp
@@ -2,28 +2,26 @@
 
 // [[Rcpp::export]]
 RawVector collage( List tiles, int width, int height, IntegerVector best_tiles, int size ){
-
-  int pixel_size = size * size ;
-  int n = 4 * width * height * pixel_size ;
+  int n = 4 * ( width * size ) * ( height * size ) ;
   RawVector out = no_init(n) ;
 
   // store the tiles into a std::vector of NumericVector so that
   // they can be used in parallel
   std::vector<RawVector> tiles_vector( tiles.begin(), tiles.end() ) ;
 
-  for( int i=0; i<width; i++){            // rows
-    int k = i*height ;
-    for( int j=0; j<height; j++, k++){    // columns
+  for( int i=0; i<height; i++){            // rows
+    int k = i*width ;
+    for( int j=0; j<width; j++, k++){    // columns
       // fill the given position with the best tile
 
-      int offset = 4 * pixel_size * i * width + 4 * j * size ;
+      int offset = i * 4 * size * width * size + 4 * j * size ;
 
-      Rbyte* p = tiles_vector[k].begin() ;
+      Rbyte* p = tiles_vector[ best_tiles[k] ].begin() ;
       Rbyte* q = out.begin() + offset ;
 
       // copy each row of the tile into the output's pixel (size*size)
       for( int ii = 0; ii < size; ii++){
-        std::copy( p, p + 4*size, q ) ;
+        std::copy( p, p + 4 * size, q ) ;
         p += 4 * size ;
         q += 4 * width * size ;
       }
