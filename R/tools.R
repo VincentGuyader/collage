dba9cf1fd1debd784fc5dc3cbc8b25985f0dc16f
Romain Francois <romain@r-enthusiasts.com>
2016-11-01 14:55:05 +0100

    redoc

--- R/tools.R
+++ R/tools.R
@@ -15,9 +15,10 @@ jpeg_to_bitmap <- function( jp ){
 
 #' Crops a square in the center of the image
 #'
-#' @param an image, typically loaded with \code{\link[magick]{image_read}} or some
+#' @param img an image, typically loaded with \code{\link[magick]{image_read}} or some
 #' other function from \code{magick}
 #' @return a square image
+#' @importFrom magick image_info image_crop
 #' @export
 image_center_crop <- function(img){
   info <- image_info(img)
