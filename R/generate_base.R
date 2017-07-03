270dd130fde1164d41303a43639fbee2358e8df4
Romain Francois <romain@thinkr.fr>
2017-07-03 23:18:10 +0200

    using purrr

--- R/generate_base.R
+++ R/generate_base.R
@@ -1,3 +1,8 @@
+# not as safe as the real thing but that's fine for now
+# just rm this when/if this PR is merged into purrr
+# https://github.com/tidyverse/purrr/pull/345
+map_raw <- function(...) unlist(purrr::map(...))
+
 #' Generates a tile base from a set of files
 #'
 #' importe le contenu du dossier de tuiles et génère un objet base
@@ -12,21 +17,18 @@
 #' }
 #' @importFrom tibble data_frame
 #' @importFrom magick image_read image_scale
+#' @importFrom purrr map
 #' @export
 generate_base <- function(files, size = 25){
   images <- image_read(files)
   scaled <- image_scale( images, "1x1!" )
 
   first <- function(.) .[[1]]
-  grab <- function(channel){
-    sapply( scaled, function(.){
-      first(.)[channel]
-    })
-  }
+  grab <- function(channel) map_raw( as.list(scaled), ~ first(.)[channel] )
 
   base     <- data_frame( R = grab(1), G = grab(2), B = grab(3))
   geometry <- sprintf( "%dx%d!", size, size )
-  tiles    <- lapply( image_scale(images, geometry), first)
+  tiles    <- map( as.list(image_scale(images, geometry)), first )
 
   structure( list( base=base, read=tiles, size = size), class ="unebase" )
 }
