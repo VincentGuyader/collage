92d694f27747d514167e4d1906da76ae783f8579
Romain Francois <romain@thinkr.fr>
2017-07-04 10:11:05 +0200

    factpr out the code that makes the geometry

--- R/tools.R
+++ R/tools.R
@@ -13,14 +13,7 @@ jpeg_to_bitmap <- function( jp ){
   out
 }
 
-#' Crops a square in the center of the image
-#'
-#' @param img an image, typically loaded with \code{\link[magick]{image_read}} or some
-#' other function from \code{magick}
-#' @return a square image
-#' @importFrom magick image_info image_crop
-#' @export
-image_center_crop <- function(img){
+image_center_crop_geometry <- function(img){
   info <- image_info(img)
   width <- info$width
   height <- info$height
@@ -32,5 +25,16 @@ image_center_crop <- function(img){
     offset <- (height - width )  / 2
     sprintf( "%dx%d+0+%d", width, width, offset)
   }
-  image_crop(img, geometry = geometry)
+  geometry
+}
+
+#' Crops a square in the center of the image
+#'
+#' @param img an image, typically loaded with \code{\link[magick]{image_read}} or some
+#' other function from \code{magick}
+#' @return a square image
+#' @importFrom magick image_info image_crop
+#' @export
+image_center_crop <- function(img){
+  image_crop(img, geometry = image_center_crop_geometry(img) )
 }
