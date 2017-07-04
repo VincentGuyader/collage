a5655453c17102591039eab46d03172d462e6d03
Romain Francois <romain@thinkr.fr>
2017-07-04 08:46:44 +0200

    rename generate_base -> tiles

--- R/generate_base.R
+++ R/generate_base.R
@@ -5,7 +5,7 @@ map_raw <- function(...) unlist(purrr::map(...))
 
 extract2 <- `[[`
 
-image_square <- function( images, size = 25){
+image_square_bitmap <- function( images, size = 25){
   geometry <- sprintf( "%dx%d!", size, size )
   image_scale(images, geometry) %>%
     as.list() %>%
@@ -14,26 +14,26 @@ image_square <- function( images, size = 25){
 
 #' Generates a tile base from a set of files
 #'
-#' importe le contenu du dossier de tuiles et génère un objet base
 #' @param files image files
 #' @param size size (width and height) of the generated tiles
 #'
+#' @return a tibble with the columns R, G, B (raw vectors) and tile
 #' @examples
 #' \dontrun{
-#'   path   <- system.file("base", package = "tipixel" )
+#'   path  <- system.file("base", package = "tipixel" )
 #'   files <- list.files( path, pattern= "jpg$", full.names = TRUE )
-#'   generate_base(files, size = 25)
+#'   tiles(files, size = 25)
 #' }
 #' @importFrom tibble tibble
 #' @importFrom magick image_read image_scale
 #' @importFrom purrr map map_df
 #' @importFrom magrittr %>%
 #' @export
-generate_base <- function(files, size = 25L){
+tiles <- function(files, size = 25L){
   images <- image_read(files)
 
-  scaled <- image_square(images, 1L)
-  tiles  <- image_square(images, size)
+  scaled <- image_square_bitmap(images, 1L)
+  tiles  <- image_square_bitmap(images, size)
   grab   <- function(i) map_raw(scaled, extract2, i)
   tibble( R = grab(1), G = grab(2), B = grab(3), tile = tiles)
 }
